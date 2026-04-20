14a23bf3466b6d992332dc97b488e609e1ef031a
DKR1688 <t.deepakrajput1688@gmail.com>
2026-04-20 11:05:06 +0530

    [Deepak] Refactored jUnit tests of auth service

--- auth-service/src/test/java/com/codesync/auth/service/AuthServiceImplTest.java
+++ auth-service/src/test/java/com/codesync/auth/service/AuthServiceImplTest.java
@@ -31,7 +31,6 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceImplTest {
-
 	private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 	@Mock
